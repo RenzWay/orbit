88515c2b7e142d83bf5549e28d3ff93935daab7a
RenzWay <narendradikaprasoca10@gmail.com>
2026-08-07 16:02:08 +0700

    FIX: fixing path native

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,12 +1,10 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import App from "../App";
 import LoginPage from "../auth/LoginPage";
 import TestPage from "../test/test";
 import { DeepLinkListener } from "../auth/DeepLinkListener";
 
-
 export function Rute() {
-    
   const path = [
     {
       path: "/",
@@ -17,13 +15,13 @@ export function Rute() {
   ];
 
   return (
-    <BrowserRouter>
+    <Router>
       <DeepLinkListener />
       <Routes>
         {path.map((item, index) => (
           <Route key={index} path={item.path} element={item.element} />
         ))}
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
