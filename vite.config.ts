e2cc7cb81ed38e395a8e8a6788417a5774f73a3e
RenzWay <narendradikaprasoca10@gmail.com>
2026-08-18 21:41:32 +0700

    third commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,19 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base:"https://github.com/RenzWay/orbit.git",
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "static/app.js",
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name?.endsWith(".css")) {
+            return "static/app.css";
+          }
+
+          return "static/[name][extname]";
+        },
+      },
+    },
+  },
 });
