7a87b360e5a92155763b442cb5c5ae9ec624cfcb
RenzWay <narendradikaprasoca10@gmail.com>
2026-09-05 16:06:51 +0700

    forgot version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 24
         targetSdk = 37
         versionCode = 1
-        versionName = "1.7.13-beta"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
